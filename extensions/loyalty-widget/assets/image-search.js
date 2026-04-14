29ef724fb8524fa09e035c19c61697c300dfd5e1
Vaishnavi Sharma <vaishnavithinkdebug2@gmail.com>
2026-04-14 11:06:14 +0530

    new

--- extensions/loyalty-widget/assets/image-search.js
+++ extensions/loyalty-widget/assets/image-search.js
@@ -166,6 +166,7 @@
         '<div class="is-empty-state">' +
           '<span class="is-empty-icon">🔍</span>' +
           '<p class="is-empty-text">No similar products found.</p>' +
+          '<p class="is-empty-hint">Try uploading a photo of a product you\'re looking for.</p>' +
           '<button class="is-retry-btn" id="is-retry-btn">Try Another Image</button>' +
         '</div>'
       );
